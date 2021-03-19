e83afced1f3a7ffb2f7d7e8f9d3651953f49fff0
rounak das <dasrubi658@gmail.com>
2021-03-19 09:02:49 +0530

    Simple Birthday Greeting Card

---
+++ app/src/main/java/com/example/bday_card/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.bday_card
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_birthday_greeting.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        
+    }
+
+    fun createBirthdayCard(view: View) {
+        val name= nameInput.editableText.toString()
+        Toast.makeText(this , "Button was clicked", Toast.LENGTH_LONG).show()
+
+        val intent= Intent(this, BirthdayGreetingActivity::class.java)
+        intent.putExtra(BirthdayGreetingActivity.NAME_EXTRA, name)
+        startActivity(intent)
+
+
+
+
+
+    }
+}
